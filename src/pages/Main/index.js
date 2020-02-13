ec3381ec6e7a36acbbadc8f4e07d6077eb194c1c
Felipe H. Teixeira <felipe@dinfo.com.br>
2020-02-13 01:01:33 -0300

    feat: 1st attempt using native speech recognition

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,111 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Voice from 'react-native-voice';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { Container } from './styles';
+import {
+  Container,
+  Info,
+  StartButton,
+  StopButton,
+  StopButtonText,
+  TranscriptContainer,
+  TranscriptText,
+  TranscriptListening
+} from './styles';
 
-const Main = ({ navigation }) => {
+const Main = () => {
+  const [started, setStarted] = useState(false);
+  const [transcript, setTranscript] = useState([]);
+
+  const onSpeechStart = e => {
+    // console.tron.log('speech start:', e);
+    setStarted(true);
+  };
+
+  const onSpeechRecognized = e => {
+    // console.tron.log('speech recognized:', e);
+  };
+
+  const onSpeechEnd = e => {
+    console.tron.log('speech end:', e);
+  };
+
+  const onSpeechError = e => {
+    // console.tron.log('speech error:', e);
+  };
+
+  const onSpeechResults = e => {
+    console.tron.log('speech results:', e);
+    if (e?.value) {
+      setTranscript(currentValue => [...currentValue, e.value]);
+    }
+  };
+
+  const onSpeechPartialResults = e => {
+    // console.tron.log('speech partial results:', e);
+  };
+
+  const onSpeechVolumeChanged = e => {
+    // console.tron.log('speech volume changed:', e);
+  };
+
+  const start = async () => {
+    try {
+      await Voice.start('pt-BR');
+    } catch (err) {
+      console.tron.error(err);
+    }
+  };
+
+  const stop = async () => {
+    try {
+      await Voice.stop();
+      setStarted(false);
+    } catch (err) {
+      console.tron.error(err);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      Voice.stop();
+      Voice.removeAllListeners();
+    } catch (err) {
+      console.tron.error(err);
+    }
+
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechRecognized = onSpeechRecognized;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechPartialResults = onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = onSpeechVolumeChanged;
+  }, []);
+
   return (
     <Container>
-      <Text>Content</Text>
-      <Button title="Sair" onPress={() => navigation.navigate('SignIn')}>
-        Sair
-      </Button>
+      {!started && (
+        <StartButton onPress={() => start()}>
+          <Icon name="microphone" size={150} color="#666" />
+          <Info>Iniciar Transcrição</Info>
+        </StartButton>
+      )}
+      {started && (
+        <>
+          <TranscriptContainer>
+            {transcript.map(t => (
+              <TranscriptText>{t}</TranscriptText>
+            ))}
+            <TranscriptListening>Ouvindo...</TranscriptListening>
+          </TranscriptContainer>
+          <StopButton onPress={() => stop()}>
+            <StopButtonText>Parar Transcrição</StopButtonText>
+          </StopButton>
+        </>
+      )}
     </Container>
   );
-};
-
-Main.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func
-  }).isRequired
 };
 
 export default Main;
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -6,4 +7,53 @@ export const Container = styled.View`
   justify-content: center;
   align-items: center;
   background: #f5f5f5;
+`;
+
+export const Info = styled.Text`
+  color: #666;
+  font-size: 22px;
+`;
+
+export const StartButton = styled(BorderlessButton)`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StopButton = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+  background: tomato;
+  border-radius: 10px;
+  justify-content: center;
+  margin: 15px 0 30px 0;
+  padding: 15px;
+  width: 100%;
+`;
+
+export const StopButtonText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+export const TranscriptContainer = styled.View`
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  background: #eee;
+  color: #666;
+  flex: 1;
+  font-size: 20px;
+  padding: 15px;
+  width: 100%;
+`;
+
+export const TranscriptText = styled.Text`
+  font-size: 22px;
+  color: #333;
+`;
+
+export const TranscriptListening = styled.Text`
+  font-size: 22px;
+  color: #666;
 `;
